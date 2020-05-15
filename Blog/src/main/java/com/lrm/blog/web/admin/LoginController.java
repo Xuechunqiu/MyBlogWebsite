85298c4bf5c01e3627c61e07a26ae12d9608533d
xuechun <noglues666@gmail.com>
2020-05-14 22:37:42 -0600

    update admin/blogs

--- Blog/src/main/java/com/lrm/blog/web/admin/LoginController.java
+++ Blog/src/main/java/com/lrm/blog/web/admin/LoginController.java
@@ -4,7 +4,6 @@ import com.lrm.blog.po.User;
 import com.lrm.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
